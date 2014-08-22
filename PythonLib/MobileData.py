5c5d8b1b0875c1310c45feda85ad4dc8bace2d39
root <516495459@qq.com>
2014-08-22 17:26:19 +0800

    add 2 test cases for testing mobile interface

---
+++ PythonLib/MobileData.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+import os
+import re
+import requests
+import json
+
+from CityData import city_data
+from BrandData import brand_data
+
+#Set the server ip according to your testing environment
+server_ip = 'http://127.0.0.1:8000/'  
+url_city = server_ip + 'mobile/city-data/'
+url_brand = server_ip + 'mobile/category/brand-data/'
+
+def _get_interface_city_data(url, key):
+    r = requests.get(url)         # get the Json data from the local testing server  
+    mobile_data = eval(r.text)           # transform the string type into dictionary
+    return mobile_data[key]
+
+
+def verify_city_data():
+    key = 'cities'
+    local_city_data = city_data['cities']
+    mobile_city_data = _get_interface_city_data(url_city, key)
+    length = len(local_city_data)
+
+    for num in range(0, length):
+        local = (u"" + local_city_data[num]['province'])
+        mobile = eval("u" + "'" + mobile_city_data[num]['province'] + "'")
+        if local == mobile:
+            print "expected value: %s, received value: %s" % (local, mobile)
+        else:
+            raise AssertionError("Provice name veification failed: expected value :%s  received value:%s" %
+            (local, mobile))
+
+    for num in range(0, length):
+        city_length = len(local_city_data[num]['cities'])
+        for i in range(0, city_length):
+            local = (u"" + local_city_data[num]['cities'][i])
+            mobile = eval("u" + "'" + mobile_city_data[num]['cities'][i] + "'")
+            if local == mobile:
+                print "expected value: %s,   received value: %s" % (local, mobile)
+            else:
+                raise AssertionError("City name veification failed: expected value :%s  received value:%s" %
+                (local, mobile))
+
+def verify_brand_data():
+    key = 'brand'
+    local_brand_data = brand_data['brand']
+    mobile_brand_data = _get_interface_city_data(url_brand, key)
+    length = len(mobile_brand_data)
+
+    for num in range(0, length):
+        local_FL = (u"" + local_brand_data[num]['first_letter'])
+        mobile_FL = eval("u" + "'" + mobile_brand_data[num]['first_letter'] + "'")
+        local_name = (u"" + local_brand_data[num]['name'])
+        mobile_name = eval("u" + "'" + mobile_brand_data[num]['name'] + "'")
+        local_slug = (u"" + local_brand_data[num]['slug'])
+        mobile_slug = eval("u" + "'" + mobile_brand_data[num]['slug'] + "'")
+
+        if local_FL == mobile_FL:
+            print "expected value: %s,   received value: %s" % (local_FL, mobile_FL)
+        else:
+            raise AssertionError("First letter veification failed: expected value :%s  received value:%s" %
+            (local_FL, mobile_FL))
+
+        if local_name == mobile_name:
+            print "expected value: %s,   received value: %s" % (local_name, mobile_name)
+        else:
+            raise AssertionError("Name veification failed: expected value :%s  received value:%s" %
+            (local_name, mobile_name))
+        if local_slug == mobile_slug:
+            print "expected value: %s,   received value: %s" % (local_slug, mobile_slug)
+        else:
+            raise AssertionError("Slug veification failed: expected value :%s  received value:%s" %
+            (local_slug, mobile_slug))      
+
+
+# This was used for testing the python lib directly
+if __name__ == '__main__':  
+    verify_city_data()
+    verify_brand_data()
--- README.md
+++ README.md
@@ -3,10 +3,19 @@ gpj-test
 
 Auto test project for gongpingjia.
 
+
 -How to use the project for testing the gongpingjia project
 1. install python on testing environment
+
 2. install robotframework on testing environment. command: pip install robotframework
-3. run the corresponding robot test case. command: pybot test_case_name.txt
+
+3. Git clone the whole testig project folder.
+
+3. Enter the TestCase folder and run the corresponding robot test case. E.g:command: pybot test_case_name.txt
+
+
+-How to show the robot-framework syntax in sublime editor?
+https://github.com/andriyko/sublime-robot-framework-assistant
 
 
 P.S:If you have any question or suggestion for the testing project, please contact Yan Xiaodong
